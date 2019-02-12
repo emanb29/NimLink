bf02de2048329131abcad97d0cfed593420e0920
emanb29 <eb@ethanbell.me>
2019-02-11 17:53:07 -0800

    Reconstruct example from [Asl16]

--- build.sbt
+++ build.sbt
@@ -8,6 +8,4 @@ scalacOptions += "-Ypartial-unification"
 
 libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-core" % "1.6.0",
-  "org.typelevel" %% "mouse" % "1.6.0",
-  "org.typelevel" %% "cats-collections" % "1.6.0",
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,58 @@
 object Main extends App {
+  sealed trait Player {
+    type Other
+  }
+  sealed trait Attacker extends Player {
+    override type Other = Defender
+  }
+  sealed trait Defender extends Player {
+    override type Other = Attacker
+  }
+
+  sealed trait BADTree[T <: Player]
+
+  final case class Action[T <: Player](/* TODO action details -- cost, probability */) extends BADTree[T]
+  final case class Conj[T <: Player](left: BADTree[T], right: BADTree[T]) extends BADTree[T]
+  final case class Disj[T <: Player](left: BADTree[T], right: BADTree[T]) extends BADTree[T]
+  final case class Counter[T <: Player](blocked: BADTree[T]) extends BADTree[T#Other]
+  final case class Complement[T <: Player](blocked: BADTree[T]) extends BADTree[T#Other]
+  final case object TRUE extends BADTree[Player] // trivially-successful action [Asl16]
+  final case object FALSE extends BADTree[Player] // trivially-failed action [Asl16]
+
+  val is: BADTree[Attacker] = Action() // identify bribable employee
+  val bs: BADTree[Attacker] = Action() // bribe employee
+
+  val t1: BADTree[Defender] = Action() // train for security
+  val tf: BADTree[Defender] = Action() // threaten to fire if bribed
+
+  val t2: BADTree[Defender] = Action() // train for not being tricked
+  val at: BADTree[Defender] = Action() // authenticate tag against forgery
+
+  val im: BADTree[Attacker] = Action() // infiltrate management
+  val ot: BADTree[Attacker] = Action() // order tag replacement
+  val st: BADTree[Attacker] = Action() // send false tag
+  val ba: BADTree[Attacker] = Action() // bypass tag authentication
+
+  val bribe: BADTree[Attacker] = Conj(is, Conj(bs, Complement(Conj(t1, tf))))
+  val b: BADTree[Attacker] = Action() // blackmail employee
+  val t: BADTree[Attacker] = Action() // trick employee
+
+  val trick = Conj(
+    Disj(
+      Conj(
+        st,
+        Complement(
+          Conj(
+            at,
+            Complement(ba)
+          )
+        )
+      ),
+      Conj(im, ot)
+    ),
+    Complement(t2)
+  )
+
+  val tree = Disj(bribe, Disj(t, Disj(b, trick)))
+
 }
